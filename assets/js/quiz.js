2b1607108ec2b8088f0013adc107998684a68d6c
graham <gr4ham18@gmail.com>
2021-12-19 18:06:12 -0500

    rules set

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,3 +6,19 @@ const sections = {
     results: document.getElementById("results"),
     highScores: document.getElementById("high-scores"),
 };
+
+// page rules 
+let questionCounter;
+let correctQuestions;
+let timeRemaining;
+let score;
+function pageLoad() {
+    timerEl.innerText = "";
+    for (item in sections) sections[item].style.display = "none";
+    sections.intro.style.display = "flex";
+    questionCounter = 0;
+    correctQuestions = 0;
+    score = 0;
+    timeRemaining = 240;
+}
+pageLoad();
