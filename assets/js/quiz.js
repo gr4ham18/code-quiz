bc66ac7f8a3c2fc4839ea4fa308f15364ede024b
graham <gr4ham18@gmail.com>
2021-12-19 18:09:05 -0500

    generate questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -26,28 +26,45 @@ pageLoad();
 // start
 function beginQuiz() {
     document.getElementById(
-      "timer-el"
+        "timer-el"
     ).innerText = `Remaining Time: ${timeRemaining}`;
     startTimer();
     questionCounter = 0;
     renderQuestion(questions[questionCounter]);
-  }
+}
 
 // timer functionality 
 function startTimer() {
     interval = setInterval(countdown, 1000);
-  }
-  
-  function countdown() {
+}
+
+function countdown() {
     if (timeRemaining <= 0) return renderResults();
     timeRemaining--;
     document.getElementById(
-      "timer-el"
+        "timer-el"
     ).innerText = `Remaining Time: ${timeRemaining}`;
-  }
-  
-  function stopTimer() {
-    clearInterval(interval);
-  }
+}
 
-  
+function stopTimer() {
+    clearInterval(interval);
+}
+
+// Generate Questions
+function renderQuestion(question) {
+    if (questionCounter >= questions.length) return renderResults();
+    sections.intro.style.display = "none";
+    sections.questionCard.style.display = "flex";
+    document.getElementById("question-notification-el").innerText = "";
+    document.getElementById("next-question-button").innerText = "Skip";
+    document.getElementById("question-text-el").innerText = question.text;
+    document.getElementById("answers-container-el").innerHTML = "";
+    for (let i = 0; i < question.answers.length; i++) {
+        let btn = document.createElement("button");
+        btn.className = "answer-choice";
+        btn.innerText = question.answers[i];
+        btn.className = "hoverable-button answer-choice";
+        btn.onclick = captureAnswer;
+        document.getElementById("answers-container-el").appendChild(btn);
+    }
+}
