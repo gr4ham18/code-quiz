895ec638f8e129438e0fb37c92bdc0ca75f00ce5
graham <gr4ham18@gmail.com>
2021-12-19 18:04:15 -0500

    contants

---
+++ assets/js/quiz.js
@@ -0,0 +1,8 @@
+// constants
+const timerEl = document.getElementById("timer-el");
+const sections = {
+    intro: document.getElementById("intro"),
+    questionCard: document.getElementById("question-card"),
+    results: document.getElementById("results"),
+    highScores: document.getElementById("high-scores"),
+};
