fa64552f50616ea7236c6262fd049e8c83c7cebf
graham <gr4ham18@gmail.com>
2021-12-19 18:02:05 -0500

    questions

---
+++ assets/js/questions.js
@@ -0,0 +1,105 @@
+const questions = [
+    {
+        text: "An HTML document can contain __?",
+        answers: [
+            "A) Attributes",
+            "B) Tags",
+            "C) Raw text",
+            "D) All the answers are true",
+        ],
+        correct: "D",
+    },
+    {
+        text: "A page designed in HTML is called _____",
+        answers: [
+            "A) Application",
+            "B) Cover Page",
+            "C) Front-end",
+            "D) Web Page",
+        ],
+        correct: "D",
+    },
+    {
+        text:
+            "An HTML document is saved with the ____ extension.",
+        answers: [
+            "A) .htl",
+            "B) .html",
+            "C) .hml",
+            "D) .htnl",
+        ],
+        correct: "B",
+    },
+    {
+        text: "The HTML document contains a root tag called ____",
+        answers: [
+            "A) Head",
+            "B) Title",
+            "C) Body",
+            "D) HTML",
+        ],
+        correct: "D",
+    },
+    {
+        text:
+            "If we want to place text around an image, which CSS property should we use?",
+        answers: [
+            "A) Push", 
+            "B) Float", 
+            "C) Align", 
+            "D) Wrap",
+        ],
+        correct: "B",
+    },
+    {
+        text: "Can we align an element by setting margin-left and margin-right?",
+        answers: [
+            "A) Yes, It's Possible",
+            "B) No, It's not possible",
+            "C) Only if you don't use Java",
+            "D) Only with Bulma",
+        ],
+        correct: "B",
+    },
+    {
+        text: "Suppose we want to arrange three DIVs so that DIV 3 is placed above DIV1. Now, which CSS property are we going to use to control the stack order?",
+        answers: [
+            "A) D-index",
+            "B) S-index",
+            "C) X-index",
+            "D) Z-index",
+        ],
+        correct: "D",
+    },
+    {
+        text: "Choose the correct HTML tag for a large title.",
+        answers: [
+            "A) H1",
+            "B) Heading",
+            "C) Head",
+            "D) H6",
+        ],
+        correct: "A",
+    },
+    {
+        text:
+            "If we want to use a nice green dotted border around an image, which css property are we going to use?",
+        answers: [
+            "A) Border-line",
+            "B) Border-style",
+            "C) Border-decoration",
+            "D) border-dotted-green-styled",
+        ],
+        correct: "B",
+    },
+    {
+        text: "What WWW stands for?",
+        answers: [
+            "A) World with Websites",
+            "B) World wild Web",
+            "C) World Wide Web",
+            "D) Nothing, is just a random thing.",
+        ],
+        correct: "C",
+    },
+];
